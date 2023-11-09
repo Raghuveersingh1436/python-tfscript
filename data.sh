25feed7dc77ee03aa0b7a8a1fd5bca0dbfdc7a55
Raghuveersingh1436 <145828543+Raghuveersingh1436@users.noreply.github.com>
2023-11-09 20:22:16 +0530

    Delete data.sh

--- data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get -y update
-sudo apt-get install -y python3-pip
-git clone https://github.com/Raghuveersingh1436/flask-library-app.git
-cd flask-library-app/
-pip3 install -r requirements.txt
-screen -m -d python3 app.py
